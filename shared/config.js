a6f7f87dc81d3a9d2f3b1a7f7a75d0f8e4ccf56c
Fernando Celmer <email@fernandocelmer.com>
2026-04-29 13:54:46 -0300

    🎨 STYLE: Move untested provider adapters to experimental category

--- shared/config.js
+++ shared/config.js
@@ -20,8 +20,9 @@
     REFRESH_PERIOD_MIN: 20,
     PROVIDERS: ["spotify", "youtube", "youtube_music", "soundcloud", "deezer", "tidal", "amazon_music", "apple_music", "bandcamp", "plex"],
     PROVIDER_CATEGORIES: {
-      players: ["spotify", "youtube", "youtube_music", "soundcloud", "deezer", "tidal", "amazon_music", "apple_music", "bandcamp", "plex"],
+      players: ["spotify", "youtube", "soundcloud", "amazon_music"],
       trackers: ["owline"],
+      experimental: ["youtube_music", "deezer", "tidal", "apple_music", "bandcamp", "plex"],
     },
     STORAGE_VERSION: 1,
   };
